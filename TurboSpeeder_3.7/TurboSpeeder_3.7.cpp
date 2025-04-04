0dd2ef551d8ac90c2b959c9baa04426d296183f8
Christoph Beickler <info@beickler.org>
2025-04-04 21:29:08 +0200

    Point2D/3D hinzugefügt

--- TurboSpeeder_3.7/TurboSpeeder_3.7.cpp
+++ TurboSpeeder_3.7/TurboSpeeder_3.7.cpp
@@ -3,12 +3,19 @@
 
 #include <iostream>
 #include <ctime>
+#include "Point2D.h"
+#include "Point3D.h"
 
 int main()
 {
     clock_t start;
     start = clock();
 
-    std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+
+    float elapsed = (float)(clock() - start) / CLOCKS_PER_SEC;
+    std::cout << "Elapsed time: " << elapsed << "\n";
+
+    return 0;
 }
 
